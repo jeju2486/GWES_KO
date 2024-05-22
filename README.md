0840664ac235ecd41e4ebd4062efadf77eacb76c
jeju2486 <jeju2486@gmail.com>
2024-05-22 12:09:58 +0100

    22-05-24

--- README.md
+++ README.md
@@ -1,5 +1,49 @@
-Hello github
+sccmec.unitigs (Result from gfa1_parser)
 
-I have changed this sentence
+0 AATGCAACGATTAGAGTCAGCAATCGCAAATAAAGATCAAACTAAAGCGAGCGAAAACTACAT
+1 GCAATGCAACGATTAGAGTCAGCAATCGCAAATAAAGATCAAACTAAAGCGAGCGAAAACTA
+2 AGATGGTGCAATGCAACGATTAGAGTCAGCAATCGCAAATAAAGATCAAACTAAAGCGAGCGAAAAC
 
-branch version.2
+scccmec.edges (Result from gfa1_parser)
+
+0 1 RR 60M
+0 344418 FF 60M
+0 551069 FF 60M
+1 2 RR 60M
+1 935664 RR 60M
+1 979397 FF 60M
+2 3 RR 60M
+3 4 RR 60M
+3 1222149 FF 60M
+4 5 RR 60M
+
+sccmec.paths (Result from gfa1_parser)
+
+sccmec_paths/1.edges
+sccmec_paths/2.edges
+sccmec_paths/3.edges
+sccmec_paths/4.edges
+sccmec_paths/5.edges
+sccmec_paths/6.edges
+sccmec_paths/7.edges
+sccmec_paths/8.edges
+
+sccmec.counts (Result from gfa1_parser)
+
+1372 2
+1373 2
+1374 2
+1375 2
+1376 2
+1377 2
+1378 2
+
+sccmec.fasta (Result from gfa1_parser)
+
+>NZ_CP039162.1
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
--- run_cuttlefish.sh
+++ run_cuttlefish.sh
@@ -3,9 +3,9 @@
 #SBATCH --job-name=test_run
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=1
 #SBATCH --partition=short
-#SBATCH --time=00-04:00:00
+#SBATCH --time=00-01:00:00
 #SBATCH --error=/data/biol-micro-genomics/kell7366/kmer_gwes/test_error.txt
 #SBATCH --output=/data/biol-micro-genomics/kell7366/kmer_gwes/test_output.txt
 
@@ -15,37 +15,38 @@ module purge
 module load Anaconda3
 source activate $DATA/newenv
 
-#make the input.txt
-find /data/biol-micro-genomics/kell7366/minimap/ref_fas/ref_aureus -type f -name '*.fas' > input.txt
+##make the input.txt
+#find /data/biol-micro-genomics/kell7366/minimap/ref_fas/ref_aureus -type f -name '*.fas' > input.txt
+#shuf -n 100 input.txt > input_100.txt
 
-##run cuttlefish ## 15mins
-cuttlefish build --list      input.txt \
-                 --kmer-len  61        \
-                 --output    sccmec     \
-                 --threads   8    \
-                 --format 1
+###run cuttlefish ## 15mins
+#cuttlefish build --list      input_100.txt \
+#                 --kmer-len  61        \
+#                 --output    sccmec     \
+#                 --threads   8    \
+#                 --format 1
+#
+## Parse the resulting Graphical Fragment Assembly (GFA) formatted file. ## 13mins
+#./PANGWES/gfa1_parser/bin/gfa1_parser sccmec.gfa1 sccmec 
 
-# Parse the resulting Graphical Fragment Assembly (GFA) formatted file. ## 13mins
-./PANGWES/gfa1_parser/bin/gfa1_parser sccmec.gfa1 sccmec 
+## Calculate mutual information scores and obtain the list of top candidate unitig pairs. ##2560.33s
+#SpydrPick --alignmentfile  sccmec.fasta \
+#            --maf-threshold  0.05           \
+#            --mi-values      50000000       \
+#            --threads        8              \
+#            --verbose
 
-# Calculate mutual information scores and obtain the list of top candidate unitig pairs.
-SpydrPick --alignmentfile  sccmec.fasta \
-            --maf-threshold  0.05           \
-            --mi-values      50000000       \
-            --threads        8              \
-            --verbose
-
-## Calculate the graph distances between unitigs in the single genome subgraphs.
-./PANGWES/unitig_distance/bin/unitig_distance --unitigs-file    sccmec.unitigs                \
-                                              --edges-file      sccmec.edges                  \
-                                              --k-mer-length    61                              \
-                                              --sgg-paths-file  sccmec.paths                  \
-                                              --queries-file    sccmec.*spydrpick_couplings*edges \
-                                              --threads         8                               \
-                                              --queries-one-based                               \
-                                              --run-sggs-only                                   \
-                                              --output-stem cdbg                                \
-                                              --verbose
+### Calculate the graph distances between unitigs in the single genome subgraphs.
+#./PANGWES/unitig_distance/bin/unitig_distance --unitigs-file    sccmec.unitigs                \
+#                                              --edges-file      sccmec.edges                  \
+#                                              --k-mer-length    61                              \
+#                                              --sgg-paths-file  sccmec.paths                  \
+#                                              --queries-file    sccmec.*spydrpick_couplings*edges \
+#                                              --threads         8                               \
+#                                              --queries-one-based                               \
+#                                              --run-sggs-only                                   \
+#                                              --output-stem sccmec                                \
+#                                              --verbose
 
 ##module load
 module purge
@@ -53,5 +54,5 @@ module load R/4.3.2-gfbf-2023a
 
 ## Draw a GWES Manhattan plot to visualize the results.
 
-Rscript ./PANGWES/scripts/gwes_plot.r /data/biol-micro-genomics/kell7366/kmer_gwes/cdbg.ud_sgg_0_based /data/biol-micro-genomics/kell7366/kmer_gwes/cdbg_sgg_mean.png 0 "Mutual information" \
-                    "" 0 "cdbg_sgg_mean (no sgg counts filtering)" \
+Rscript ./PANGWES/scripts/gwes_plot.r /data/biol-micro-genomics/kell7366/kmer_gwes/sccmec.ud_sgg_0_based /data/biol-micro-genomics/kell7366/kmer_gwes/sccmec_sgg_mean.png 0 "Mutual information" \
+                    "" 0 "sccmec_sgg_mean (no sgg counts filtering)" \
